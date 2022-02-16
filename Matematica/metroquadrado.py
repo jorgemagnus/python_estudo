b948f323ae4a2a9f58510ebbdb5518be40ee591c
jorgemagnus <jorgemagnus_rn@hotmail.com>
2022-02-16 11:23:01 -0300

    Inserido calculo de metro quadrado

---
+++ Matematica/metroquadrado.py
@@ -0,0 +1,12 @@
+'''
+ Calculando o metro quadrado.
+ formula: m = comprimento * largura
+'''
+
+largura = float(input('Informe a largura:'))
+comprimento = float(input('Informe o comprimento:'))
+
+
+m = comprimento * largura
+
+print(f'O valor em metros quadrados é: {m}')
